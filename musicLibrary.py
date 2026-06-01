722f2cabb3a4b94ee848c53ab173d0699986bfeb
Harman Bhuju <harmanbhuju@gmail.com>
2026-06-01 17:03:19 +0545

    music

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,4 +1,4 @@
 music = {
-    "bardali" : "https://youtu.be/HAcLoqZO-Z0?si=rfSXmH4tz8rDmABC", #bardali
+    "balcony" : "https://youtu.be/HAcLoqZO-Z0?si=rfSXmH4tz8rDmABC", #bardali
     "forget" : "https://youtu.be/H7bzyggFYSE?si=16dX9Ujai02CGJ96" #samjine hau ki
 }
